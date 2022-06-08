d5e2aa5438daf704f672561c2ad05e4fd5d95c20
ulviabdullazada <ulvi.abdullazada@code.edu.az>
2022-06-09 00:48:49 +0400

    first commit

---
+++ AzTUChat/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AzTUChat.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
